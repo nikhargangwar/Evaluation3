8c4ac13f68b221b19f610278c1d62b256d421485
nikhar <nikhar.gangwar13@gmail.com>
2023-03-01 10:47:45 +0530

    chore: add initial files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+// import {
+//   ERROR_ROUTE,
+//   GENRES_ROUTE,
+//   HOME_ROUTE,
+//   SONGS_ROUTE,
+// } from './constants/routes';
+// import {
+//   ErrorPage,
+//   GenresPage,
+//   HomePage,
+//   NotFoundPage,
+//   SongsPage,
+// } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/songs" element={<Songs />} />
+        <Route path="/genre" element={<SongsByGenre />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Navbar from '../../components/Navbar';
+// import SyncButton from '../../components/SyncButton';
+
+function Home() {
+  return (
+    <div className="app">
+      {/* <Navbar />
+      <SyncButton /> */}
+      inasln
+    </div>
+  );
+}
+
+export default Home;
